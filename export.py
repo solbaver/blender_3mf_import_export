65198e0b03277664c3d61e6d9e78ade765294365
Solbaver <solbaver@gmail.com>
2016-08-26 21:33:14 -0500

    Add files via upload
    first version of import and export
    runs from console:
    blender --background --python SCRIPT_NAME 'YOUR_FILE_PATH' 'YOUR_FILE_NAME'

---
+++ export.py
@@ -0,0 +1,145 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+import zipfile
+import sys
+import os
+import shutil
+
+#just type in console:
+#blender --background --python export.py 'YOUR_FILE_PATH' 'YOUR_FILE_NAME'
+
+unit = 'millimeter'
+name = 'test'
+
+filepath = sys.argv[4]
+filename = sys.argv[5]
+
+print (filename)
+print (filepath)	
+bpy.ops.wm.open_mainfile(filepath=filepath+filename)
+
+def create_types():
+	t = open("[Content_Types].xml", 'w')
+	t.write('''<?xml version="1.0" encoding="UTF-8"?>''')
+	t.write('''<Types>''')
+	t.write('''<Default ContentType="application/vnd.openxmlformats-package.relationships+xml" Extension="rels"/>''')
+	t.write('''<Default ContentType="application/vnd.ms-package.3dmanufacturing-3dmodel+xml" Extension="model"/>''')
+	t.write('''</Types>''')
+	t.close()	
+	
+def find_transformation(ob):	
+	if ob.type == 'MESH':
+			
+			old_matrix = list(ob.matrix_world)
+			print (ob.name)
+			new_matrix = ''
+			for line in old_matrix:
+				new_matrix = new_matrix + str(line[0]) + ' ' +  str(line[1]) + ' ' +  str(line[2]) + ' '
+			print (new_matrix)
+			
+
+			#t.write(matrix_output)
+
+			return new_matrix
+def create_object(ob, i, t):			
+
+	t.write('''<object id="''' + str(i) +  '''" name="''' + str(ob.name) + '''" type="model">''' + '\n')
+	t.write('''<mesh>''' + '\n')
+
+	#print (coords)
+	#print (len(coords))
+	t.write('''<vertices>''' + '\n')
+	vert_coords = [(ob.matrix_world * v.co) for v in ob.data.vertices]
+	for c in vert_coords:
+		t.write('''<vertex x="''' + str(c[0]) + '''" y="''' + str(c[1]) + '''" z="''' + str(c[2]) + '''"/>''' + '\n')
+	#t.write('''<TEST id="''' + str(i) +  '''" name="''' + str(ob.name) + '''" type="model"/>''' + '\n')
+	t.write('''</vertices>''' + '\n')
+	
+	t.write('''<triangles>''' + '\n')
+	poly_coords = [(p.vertices[:]) for p in ob.data.polygons]
+	for c in poly_coords:
+		t.write('''<triangle v1="''' + str(c[0]) + '''" v2="''' + str(c[1]) + '''" v3="''' + str(c[2]) + '''"/>''' + '\n')
+	#t.write('''<TEST id="''' + str(i) +  '''" name="''' + str(ob.name) + '''" type="model"/>''' + '\n')
+	t.write('''</triangles>''' + '\n')
+	
+	t.write('''</mesh>''' + '\n')
+	t.write('''</object>''' + '\n')
+	
+	
+def create_model():
+	scene = bpy.context.scene
+	t = open(filepath + "3D" "/3dmodel.model", 'w')
+	t.write('''<?xml version="1.0" encoding="UTF-8"?>''' + '\n')
+	t.write('''<model unit="''' + str(unit) + '''" xml:lang="en-US" xmlns="http://schemas.microsoft.com/3dmanufacturing/core/2015/02">''' + '\n')
+	t.write('''<metadata name="Title">''' + name + '''</metadata>''' + '\n')
+	t.write('''<metadata name="Designer">''' + name + '''</metadata>''' + '\n')
+	t.write('''<metadata name="LicenseTerms">''' + name + '''</metadata>''' + '\n')
+	t.write('''<metadata name="CreationDate">''' + name + '''</metadata>''' + '\n')
+	t.write('''<metadata name="Description">''' + name + '''</metadata>''' + '\n')
+	t.write('''<resources>''' + '\n')
+	i = 1
+	for ob in scene.objects:
+		if ob.type == 'MESH':
+			create_object(ob, i, t)
+			i = i + 1
+	#
+	t.write('''</resources>''' + '\n')
+		
+	t.write('''<build>''' + '\n')
+	i = 1
+	for ob in scene.objects:
+		if ob.type == 'MESH':
+			new_matrix = find_transformation(ob)
+			line = '''<item objectid="''' + str(i) + '''" transform="''' + str(new_matrix) + '''"/>''' + '\n'
+			t.write(line)
+			i = i + 1
+	t.write('''</build>''' + '\n')
+
+	t.write('''</model>''' + '\n')
+	
+	t.close
+
+	
+	
+for param in sys.argv:
+	print (param)
+create_types()
+
+os.mkdir(filepath+'/_rels')
+os.mkdir(filepath+'/3D')
+os.chdir(filepath+'/3D') 
+
+create_model()
+
+#creating .3dm archive
+os.chdir(filepath) 
+zip_name = filename + '.3mf'
+zip_exp = zipfile.ZipFile(filepath + zip_name, mode='w')
+zip_exp.write('[Content_Types].xml')
+zip_exp.write('_rels')
+zip_exp.write('3D/3dmodel.model')
+
+zip_exp.close()
+
+#deleting leftovers
+os.remove(filepath+'[Content_Types].xml')
+os.remove(filepath+'3D/3dmodel.model')
+os.rmdir(filepath+'3D')
+os.rmdir(filepath+'_rels')
---
+++ import.py
@@ -0,0 +1,172 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy
+import zipfile
+from mathutils import Matrix
+from bs4 import BeautifulSoup
+import sys
+import re
+import os
+import shutil
+
+#just type in console:
+#blender --background --python import.py 'YOUR_FILE_PATH' 'YOUR_FILE_NAME'
+
+
+
+for i in sys.argv:
+	print (i)
+
+
+filepath = sys.argv[4]
+filename = sys.argv[5]
+modelpath = '/model'
+zip_ref = zipfile.ZipFile(filepath + filename, 'r')
+zip_ref.extractall(filepath + modelpath)
+zip_ref.close()
+
+model_file = open(filepath + modelpath + '/3D/' + '3dmodel.model', 'r')
+
+soup = BeautifulSoup(model_file, features="xml")
+
+#number of objects in scene
+objects_number = len(soup.resources.findAll("object"))    
+
+
+num = 1
+
+unit = soup.model['unit']
+print (unit)
+if unit == 'micron':
+	bpy.context.scene.unit_settings.system = 'METRIC'
+	bpy.context.scene.scale_length = 0.001
+if unit == 'millimeter':
+	bpy.context.scene.unit_settings.system = 'METRIC'
+	bpy.context.scene.unit_settings.scale_length = 0.01
+if unit == 'centimeter':
+	bpy.context.scene.unit_settings.system= 'METRIC'
+	bpy.context.scene.scale_length = 0.1
+if unit == 'meter':
+	bpy.context.scene.unit_settings.system= 'METRIC'
+	bpy.context.scene.scale_length = 1
+if unit == 'foot':
+	bpy.context.scene.unit_settings.system = 'IMPERIAL'
+	bpy.context.scene.scale_length = 1	
+if unit == 'inch':
+	bpy.context.scene.unit_settings.system = 'IMPERIAL'
+	bpy.context.scene.scale_length = 0.1	
+##mi
+
+
+while num < objects_number:
+#id - number of current object
+	ob = soup.resources.findAll(id=num)
+	for n in ob:
+		name = n.get('name')
+		print (name)
+
+	vetexes = []
+	triangles = []
+
+##peredelat nahui!!!!
+	ob = BeautifulSoup(str(ob)[1:-1], features="xml")
+	vertex = ob.findAll('vertex')
+	triangle = ob.findAll('triangle')
+	ob = soup.resources.findAll(id=num)
+#part for matrix calculations
+	ob_transform = soup.build.findAll(objectid=num)
+	ob_transform = BeautifulSoup(str(ob_transform)[1:-1])
+	transform = ob_transform.item['transform']
+
+	N = map(float,transform.split())
+	N = list(N)
+
+	matrix = [(N[0], N[1], N[2], 0), (N[3], N[4], N[5], 0), (N[6], N[7], N[8], 0), (N[9], N[10], N[11], 1.0)]
+	matrix = Matrix(matrix)
+
+	vertexes_x = []
+	vertexes_y = []
+	vertexes_z = []
+
+	triangles_1 = []
+	triangles_2 = []
+	triangles_3 = []
+ 
+	verts = []
+	faces = []
+ 
+	for v in vertex:
+		v = BeautifulSoup(str(v), features="xml")
+		vertexes_x.append(v.vertex['x'])
+		vertexes_y.append(v.vertex['y'])
+		vertexes_z.append(v.vertex['z'])
+
+	for t in triangle:
+		t = BeautifulSoup(str(t), features="xml")
+		triangles_1.append(t.triangle['v1'])
+		triangles_2.append(t.triangle['v2'])
+		triangles_3.append(t.triangle['v3'])
+	
+	i = 0	
+		##peredelat nahui!!!!!!
+	while i < len(vertexes_x):
+		vertex_tuple = (float(vertexes_x[i]), float(vertexes_y[i]), float(vertexes_z[i]))	
+		verts.insert(i, vertex_tuple)
+		i = i + 1
+	verts = tuple(verts)
+	#print (verts)
+
+	i = 0	
+	while i < len(triangles_1):
+		triangle_tuple = (int(triangles_1[i]), int(triangles_2[i]), int(triangles_3[i]))	
+		faces.insert(i, triangle_tuple )
+		i = i + 1
+
+	faces = tuple(faces)
+	#print (faces)
+
+	mesh_name = str(num)
+	#creating object in blender
+	blender_me = bpy.data.meshes.new(name)
+	blender_ob = bpy.data.objects.new(name, blender_me)
+	scn = bpy.context.scene
+	scn.objects.link(blender_ob)	
+	scn.objects.active = blender_ob
+	blender_ob.select = True
+
+	#	mesh = bpy.data.meshes.new(mesh_name)
+	#mesh.from_pydata(verts, [], faces)
+
+	blender_me.from_pydata(verts, [], faces)
+
+	blender_me.update(calc_edges=True)
+	num = num + 1
+
+	blender_ob.matrix_world = matrix * blender_ob.matrix_world 
+	#print (blender_ob.matrix_world)
+
+    
+model_file.close()
+bpy.ops.wm.save_as_mainfile(filepath=filepath+filename+'.blend')
+
+
+rempath = os.path.join(os.path.abspath(os.path.dirname(filepath)), 'model')
+
+shutil.rmtree(rempath)
+
